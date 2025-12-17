5ee21956b3f43367b940eab32dc0bfd47c5fb9bd
dayanas3na <dayanabsena0@gmail.com>
2025-12-17 00:20:11 -0300

    solução do desafio dia 1

--- dia1/main.py
+++ dia1/main.py
@@ -1,1 +1,16 @@
-print("hello word")
+# Criação de uma calculadora simples 
+
+#variáveis
+primeiro_numero = int(input("Digite o primeiro número:"))
+segundo_numero = int(input("Digite o segundo número:"))
+
+soma = primeiro_numero + segundo_numero
+subtracao = primeiro_numero - segundo_numero
+multiplicacao = primeiro_numero * segundo_numero
+divisao = primeiro_numero / segundo_numero
+
+print(f"O resultado da soma é: {soma}")
+print(f"O resultado da subtração é: {subtracao}")
+print(f"O resultado da multiplicação é: {multiplicacao}")
+print(f"O resultado da divisão é: {divisao}")
+
